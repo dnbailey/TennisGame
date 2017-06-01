e4c71329aec8cf71c1a7527e68de47045b7b5f4c
dnbailey <dev@dnbailey.org>
2017-06-01 15:14:02 -0400

    Draw canvas background

--- js/tennis-game.js
+++ js/tennis-game.js
@@ -1,1 +1,19 @@
 // Tennis Game
+
+// Initiate canvas
+let canvas;
+let canvasContext;
+
+
+window.onload = function() {
+  canvas = document.getElementById('game');
+  canvasContext = canvas.getContext('2d');
+
+  drawGame();
+}
+
+function drawGame() {
+  // Canvas background
+  canvasContext.fillStyle = 'black';
+  canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+}
