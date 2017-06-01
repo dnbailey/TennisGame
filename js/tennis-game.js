db486e413f6de4124980109a8b53213a4b862703
dnbailey <dev@dnbailey.org>
2017-06-01 15:46:43 -0400

    Draw ball, paddles, and net

--- js/tennis-game.js
+++ js/tennis-game.js
@@ -4,6 +4,9 @@
 let canvas;
 let canvasContext;
 
+// Paddles
+let PADDLE_WIDTH = 10;
+let PADDLE_HEIGHT = 100;
 
 window.onload = function() {
   canvas = document.getElementById('game');
@@ -14,7 +17,21 @@ window.onload = function() {
 
 function drawGame() {
   // Canvas background
-  drawRect(0, 0, canvas.width, canvas.height, 'black');
+  drawRect(0, 0, canvas.width, canvas.height, 'rgb(40,40,40)');
+
+  // Draw net
+  for (i = 0; i < canvas.height; i += 40) {
+    drawRect(canvas.width / 2 - 1, i, 2, 20, 'white');
+  };
+
+  // Draw ball
+  drawRect(250, 250, 10, 10, 'rgb(51,255,0)');
+
+  // Draw Player Paddle
+  drawRect(0, 250, PADDLE_WIDTH, PADDLE_HEIGHT, 'white');
+
+  // Draw Comp Paddle
+  drawRect(canvas.width-10, 250, PADDLE_WIDTH, PADDLE_HEIGHT, 'white');
 }
 
 function drawRect(top, left, width, height, color) {
